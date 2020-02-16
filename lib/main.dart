9941fb4e6b04024cc815d706c001f7d4ebc18672
Eyzo280 <48450982+Eyzo280@users.noreply.github.com>
2020-02-16 23:05:01 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart!'),
+              color: Colors.blue,
+              elevation: 5, // Wielkosc cienia
+            ),
+          ),
+          Card(
+            child: Text('data'),
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
